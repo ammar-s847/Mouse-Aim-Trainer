2931e1bdf71e2c6da53d9328d2ecffa4c627ce70
Ammar Siddiqui <ammar.siddiqui847@gmail.com>
2020-08-17 09:52:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mouse Aim Trainer
+A <b>Python</b> game built with <b>PyGame</b> where the user can improve their mouse aim by clicking on randomly generated targets.
+<br>Inspired by <a href="http://www.aimbooster.com/">aimbooster.com</a>
---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame
+import time
+import math
+import random
+from sys import exit as sysExit
+
+# Initialization
+pygame.mixer.pre_init(44100, 16, 2, 2048)
+pygame.init()
+
+WinX, WinY = 450, 450
+window = pygame.display.set_mode((WinX, WinY))
+pygame.display.set_caption("Mouse Aim Trainer")
+run = True
+clock = pygame.time.Clock()
+
+# Colours
+
+
+# Classes
+
+
+# Variables
+
+
+# Functions
+
+
+# PyGame Loop
+while run:
+    # Event Detection
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+if not run:
+    pygame.quit()
+    sysExit()
